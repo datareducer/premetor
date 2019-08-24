6a65480fa985bfe33b39c1b55f8b4edbed368fc6
Kirill <kamikhailov@gmail.com>
2019-08-24 11:15:55 +0300

    Анализ замеров времени выполнения ключевых операций

--- measurements.R
+++ measurements.R
@@ -3,19 +3,32 @@
 library(XML)
 library(data.table)
 
-file <- '~/1c_perfomance/2019-08-18 16-18-17-00001.xml'
+files <- list.files(path = "~/1c_perfomance", pattern = ".*\\.xml$", full.names = TRUE, recursive = TRUE)
 
-xml <- xmlParse(file)
+xml2dt <- function(file) {
+  xml <- xmlParse(file)
+  nsps = xmlNamespaceDefinitions(xml, simplify = TRUE);
+  ns <- getNodeSet(xml, '//prf:KeyOperation', namespaces = nsps)
+  
+  result <- rbindlist(lapply(ns, function(x) {
+    op <- xpathSApply(x, ".", xmlAttrs, namespaces = nsps);
+    # Требуется установка кодировки, её указание в параметре xpathSApply не решает проблему:
+    Encoding(op) <- "UTF-8"; 
+    op <- data.frame(t(op));
+    
+    me <- xpathSApply(x, ".//prf:measurement", xmlAttrs, namespaces = nsps);
+    Encoding(me) <- "UTF-8";
+    me <- data.frame(t(me));
+    
+    cbind(op, me);
+  }))
+  
+  return(result);
+}
 
-nsps = xmlNamespaceDefinitions(xml, simplify = TRUE);
-
-ns <- getNodeSet(xml, '//prf:KeyOperation', namespaces = nsps)
-
-meas_dt <- rbindlist(lapply(ns, function(x) {
-  op <- data.frame(t(xpathSApply(x, ".", xmlAttrs, namespaces = nsps )))
-  me <- data.frame(t(xpathSApply(x, ".//prf:measurement", xmlAttrs, namespaces = nsps )))
-  cbind(op, me)
-}))
+meas_dt <- rbindlist(
+  lapply(files, xml2dt)
+)
 
 
 
